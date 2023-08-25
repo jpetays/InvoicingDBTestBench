fd93c4846d42ddb8d06b282a6e9d8f7a02506d99
Jari Petays <jari.petays@iki.fi>
2023-08-25 08:26:30 +0300

    add logging support: Apache Log4j SLF4J API binding to Log4j 2 Core

--- src/main/java/petays/ShowCurrentData.java
+++ src/main/java/petays/ShowCurrentData.java
@@ -1,30 +1,36 @@
 package petays;
 
 import homebeach.DataGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.SQLException;
 
 public class ShowCurrentData {
+    private static final Logger logger = LoggerFactory.getLogger(ShowCurrentData.class);
+
     public static void main(final String[] args) throws SQLException {
 
         final DataGenerator dataGenerator = new DataGenerator(Config.sql_databases, Config.neo4j_settings, Config.db_mariadb_url);
 
-        System.out.println("Get row counts");
+        String[] db_settings = Config.sql_databases.get(Config.db_mariadb_url);
+
+        logger.debug("Get SQL row counts from {}", Config.db_mariadb_url);
 
         final var customerCount = dataGenerator.getCustomerCount();
         final var lastCustomerId = dataGenerator.getLastCustomerId();
-        System.out.println("customerCount " + customerCount + " lastCustomerId " + lastCustomerId);
+        logger.debug("customerCount {} lastCustomerId {}", customerCount, lastCustomerId);
         final var invoiceCount = dataGenerator.getInvoiceCount();
         final var lastInvoiceId = dataGenerator.getLastInvoiceId();
-        System.out.println("invoiceCount " + invoiceCount + " lastInvoiceId " + lastInvoiceId);
+        logger.debug("invoiceCount {} lastInvoiceId {}", invoiceCount, lastInvoiceId);
         final var itemCount = dataGenerator.getItemCount();
         final var lastItemId = dataGenerator.getLastItemId();
-        System.out.println("itemCount " + itemCount + " lastItemId " + lastItemId);
+        logger.debug("itemCount {} lastItemId {}", itemCount, lastItemId);
         final var workCount = dataGenerator.getWorkCount();
         final var lastWorkId = dataGenerator.getLastWorkId();
-        System.out.println("workCount " + workCount + " lastWorkId " + lastWorkId);
+        logger.debug("workCount {} lastWorkId {}", workCount, lastWorkId);
         final var workTypeCount = dataGenerator.getWorkTypeCount();
         final var lastWorkTypeId = dataGenerator.getLastWorkTypeId();
-        System.out.println("workTypeCount " + workTypeCount + " lastWorkTypeId " + lastWorkTypeId);
+        logger.debug("workTypeCount {} lastWorkTypeId {}", workTypeCount, lastWorkTypeId);
     }
 }
